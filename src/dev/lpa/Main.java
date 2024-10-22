a97fe47b223f8364fd8b956887cc5b835a417f8f
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-22 14:23:04 +0200

    rename oldFile.renameTo(newFile) if exists

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,9 +1,18 @@
 package dev.lpa;
 
+import java.io.File;
+
 public class Main {
   
   public static void main(String[] args) {
-  
-  
+    
+    File oldFile = new File("students.json");
+    File newFile = new File("student-activity.json");
+    if (oldFile.exists()) {
+      oldFile.renameTo(newFile);  // IO package, returns boolean, doesn't throw IOException
+      System.out.println("File renamed successfully!");
+    } else {
+      System.out.println("File does not exist!");
+    }
   }
 }
