61d283c692f6d4d01ee0d67d44867060b173b76c
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-22 14:39:58 +0200

    show Files.move() and Files.createDirectories()

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,6 +1,5 @@
 package dev.lpa;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,21 +8,25 @@ public class Main {
   
   public static void main(String[] args) {
     
-    File oldFile = new File("students.json");
-    File newFile = new File("student-activity.json");
-    if (oldFile.exists()) {
-      oldFile.renameTo(newFile);  // IO package, returns boolean, doesn't throw IOException
-      System.out.println("File renamed successfully!");
-    } else {
-      System.out.println("File does not exist!");
-    }
+//    File oldFile = new File("students.json");
+//    File newFile = new File("student-activity.json");
+//    if (oldFile.exists()) {
+//      oldFile.renameTo(newFile);  // IO package, returns boolean, doesn't throw IOException
+//      System.out.println("File renamed successfully!");
+//    } else {
+//      System.out.println("File does not exist!");
+//    }
     
-    Path oldPath = oldFile.toPath();
-    Path newPath = newFile.toPath();
+//    Path oldPath = oldFile.toPath();
+//    Path newPath = newFile.toPath();
+    
+    Path oldPath = Path.of("students.json");
+    Path newPath = Path.of("files/student-activity.json");
     
     try {
-      Files.move(newPath, oldPath);
-      System.out.println("Path renamed successfully.");
+      Files.createDirectories(newPath.subpath(0, newPath.getNameCount() - 1));
+      Files.move(oldPath, newPath);
+      System.out.println(oldPath + " moved (renamed to) --> " + newPath);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
